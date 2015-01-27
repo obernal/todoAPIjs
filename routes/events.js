37cd276b17201eb5d72469b822d241caa9b79dbe
Oscar Bernal <oscar.bernal@gmail.com>
2015-01-27 17:20:57 -0200

    simplified for simple logging tests

---
+++ routes/events.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var bunyan = require('bunyan');
+var log = bunyan.createLogger({name: "myapp",
+streams: [
+{
+  level: 'info',
+  stream: process.stdout            // log INFO and above to stdout
+},
+{
+  level: 'info',
+  path: 'events.log'  // log ERROR and above to a file
+}
+]});
+
+/* POST /events */
+router.post('/', function(req, res, next) {
+  log.info(req.body);
+  res.json({status:"ok"});
+});
+
+module.exports = router;
